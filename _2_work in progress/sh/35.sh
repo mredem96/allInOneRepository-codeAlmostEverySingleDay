2b4ddf2ebfb03a371108be77633104c0f82f98fd
((())) <a____@MacBook-Pro-de-.local>
2017-04-20 14:36:30 +0100

    addinglearningconents

---
+++ _2_work in progress/sh/35.sh
@@ -0,0 +1,27 @@
+git clone library of cpp math expressions
+git clone developing a cpp algorithmic composition framework
+git clone https://github.com/hackerkid/Mind-Expanding-Books
+git clone https://github.com/khublou/Curriculum-Bachelors-CS
+git clone https://github.com/open-source-society/computer-science
+git clone https://github.com/open-source-society/data-science
+git clone https://github.com/open-source-society/help
+git clone https://github.com/open-source-society/bioinformatics
+git clone https://github.com/open-source-society/ossu-client
+git clone https://github.com/open-source-society/game-development
+git clone https://github.com/open-source-society/ossu-api
+git clone https://github.com/open-source-society/ossu-ui
+git clone https://github.com/prakhar1989/awesome-courses
+git clone https://github.com/khublou/curated-programming-resources
+git clone https://github.com/khublou/awesome
+git clone https://learnpythonthehardway.org/book/
+git clone https://github.com/getify/You-Dont-Know-JS
+git clone https://github.com/qmlbook/qmlbook
+git clone https://github.com/tiagmoraismorgado/machine-learning
+git clone https://github.com/tiagmoraismorgado/freeCodeCamp
+git clone https://github.com/HandsOnOpenCL
+git clone https://github.com/smistad/OpenCL-Getting-Started
+git clone https://github.com/dcousens/OpenCL
+git clone https://github.com/jamolnng/OpenCL-CUDA-Tutorials
+git clone https://github.com/tkhubert/OpenCL
+git clone https://github.com/CNugteren/myGEMM
+git clone https://github.com/gcielniak/OpenCL-Tutorials
