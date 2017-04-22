2b02a9444d9de7a8705974cdcf99ae57c4ed4177
((())) <a____@MacBook-Pro-de-.local>
2017-04-22 15:33:27 +0100

    prometheusScale

--- _1_mainFiles/1.cpp
+++ _1_mainFiles/1.cpp
@@ -18,6 +18,9 @@ int augmentedScale[12] = {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1};
 int alteredScale[12] = {1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0};
 int blueScale[12] = {1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0};
 int slendroScale[12] = {1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0};
+int yoScale[12] = {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0};
+int yoScale[12] = {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0};
+int prometheusScale[12] = {1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0};
 
 int copyPrintArray[12];
 
@@ -56,6 +59,8 @@ int printScale() {
     cout << endl << "o - print altered scale";
     cout << endl << "p - print blues scale";
     cout << endl << "q - print slendro scale";
+    cout << endl << "r - print yo scale";
+    cout << endl << "s - print prometheus scale";
     cout << endl << "0 - quit";
 
     cin >> op;
@@ -111,6 +116,12 @@ int printScale() {
         break;
       case 'q':
         copy(begin(slendroScale), end(slendroScale), begin(copyPrintArray));
+        break;
+      case 'r':
+        copy(begin(yoScale), end(yoScale), begin(copyPrintArray));
+        break;
+      case 's':
+        copy(begin(prometheusScale), end(prometheusScale), begin(copyPrintArray));
         break;
       default:
         break;
