1a1c3f8b87861915fbfd3574277581e5f0fdf731
tiagmoraismorgado <eremitatm88@gmail.com>
2017-05-05 18:34:26 +0100

    3-5-5-17

--- _1_mainFiles/cppClasses/cppOperationsOnVectors.cpp
+++ _1_mainFiles/cppClasses/cppOperationsOnVectors.cpp
@@ -6,19 +6,29 @@ int myIntList[4] = {1, 2, 3, 4};
 class vectorOperations {
 	public:
 		int sumRange();
+        long double mulRange();
 };
 
 
 int vectorOperations::sumRange() {
 	int result = 0;
-	for(int i=0; i<sizeof(myIntList); i++) {result += myIntList[i];} 
+	for(int i=0; i<4; i++) {result += myIntList[i];}
 	cout << endl << result;	
 	return 0;
 }
 
+long double vectorOperations::mulRange() {
+    long double result = 1;
+    for(int i=0; i<4; i++) {result = result * myIntList[i];}
+    cout << endl << result;
+    return 0;
+}
+
+
 int main() {
 	vectorOperations myVect;
 	myVect.sumRange();
+    myVect.mulRange();
 	return 0;
 }
 
