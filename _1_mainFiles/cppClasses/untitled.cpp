b4c793fcee51877bc6d77701108fd764f4012b5a
Tiago Morais Morgado <eremitatm88@gmail.com>
2017-05-23 17:10:06 +0100

    hashcat cpp without dictionary

---
+++ _1_mainFiles/cppClasses/untitled.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+char cena[13];
+
+int main()
+{
+    for(int i = 33; i < 127; i++) {
+    for(int i1 = 33; i1 < 127; i1++) {
+    for(int i2 = 33; i2 < 127; i2++) {
+   	for(int i3 = 33; i3 < 127; i3++) {
+    for(int i4 = 33; i4 < 127; i4++) {
+   	for(int i5 = 33; i5 < 127; i5++) {
+	for(int i6 = 33; i6 < 127; i6++) {
+    for(int i7 = 33; i7 < 127; i7++) {
+   	for(int i8 = 33; i8 < 127; i8++) {
+   	for(int i9 = 33; i9 < 127; i9++) {
+   	for(int i10 = 33; i10 < 127; i10++) {
+   	for(int i11 = 33; i11 < 127; i11++) {
+   	for(int i12 = 33; i12 < 127; i12++) {
+   		cena[0] = (char)i;
+   		cena[1] = (char)i1;
+   		cena[2] = (char)i2;
+   		cena[3] = (char)i3;
+   		cena[4] = (char)i4;
+   		cena[5] = (char)i5;
+   		cena[6] = (char)i6;
+   		cena[7] = (char)i7;
+   		cena[8] = (char)i8;
+   		cena[9] = (char)i9;
+   		cena[10] = (char)i10;
+   		cena[11] = (char)i11;
+   		cena[12] = (char)i12;
+    cout << endl << cena << endl;
+    }}}}}}}}}}}}}
+    return 0;
+}
