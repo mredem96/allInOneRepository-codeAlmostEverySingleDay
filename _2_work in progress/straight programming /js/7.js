7a6ea2d672084770d6131defc11d8de9e67a07bf
Tiago Morais Morgado <eremitatm88@gmail.com>
2017-06-17 18:57:35 +0100

    Add files via upload

---
+++ _2_work in progress/straight programming /js/7.js
@@ -0,0 +1,9 @@
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+console.log(datetime);
