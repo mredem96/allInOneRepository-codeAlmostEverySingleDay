509992179102b52aebeb2c7dbf652d8fb39f2aa8
Tiago Morais Morgado <eremitatm88@gmail.com>
2017-06-22 12:43:52 +0100

    Add files via upload

---
+++ _1_mainFiles/Fibonacci.java
@@ -0,0 +1,24 @@
+public class Fibonacci
+{
+	public static void Fibonacci() {
+			int dim=10; int varA=1; int varB=1; int result = 0;
+			result = varA+varA;
+			for(int i = 0; i < dim; i++) {
+			varB=result; result += varB; varA=varB;
+			System.out.println("\n fibonacci result = " + result);
+		}
+	}
+
+	public static void Factorial() {
+			int result = 1; int factorial = 8;
+			for(int i = 1; i <= factorial; i++) {
+				result*=factorial;
+				System.out.println("\n factorial result = " + result);				
+			}
+		}		
+
+	public static void main(String[] args) {
+			Fibonacci();
+			Factorial();
+	}
+}
